2b265e6fcede27fe4c8cb3163a0db016ade82750
albertot_dev <atejero.dam@gmail.com>
2020-10-09 21:11:09 +0200

    clean files unnecesary :)

--- projects/postal-code-autocomplete/src/public-api.ts
+++ projects/postal-code-autocomplete/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of postal-code-autocomplete
  */
 
-export * from './lib/postal-code-autocomplete.service';
 export * from './lib/postal-code-autocomplete.component';
 export * from './lib/postal-code-autocomplete.module';
 export * from './lib/es';
